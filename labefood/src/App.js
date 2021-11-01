82b7a87cde90b45674b6ace31cd49a37b7e91d63
nomytalo <email>
2021-11-01 16:58:39 -0300

    Testando o Material-Ui

--- labefood/src/App.js
+++ labefood/src/App.js
@@ -2,6 +2,7 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
 import { Router } from './routers/Router';
+import Button from '@material-ui/core/Button';
 
 
 export default function App() {
@@ -9,6 +10,9 @@ export default function App() {
     <div>
       <Typography variant={'h1'}>FutureEats</Typography>
       <Router/>
+      <Button variant="contained" color="primary">
+          Primary
+      </Button>
     </div>
   );
 }
