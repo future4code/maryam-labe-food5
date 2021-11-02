31353c253772a2d19d3439cd4c2e8e6fdedb6cd0
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-11-02 16:24:26 -0300

    inclui hooks reutilizaveis

---
+++ labefood/src/hooks/useRequestData.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+const useRequestData = (initialData, url) => {
+    const [data, setData] = useState(initialData)
+
+    useEffect(() => {
+        axios.get(url, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+                alert("Ocorreu um erro")
+            })
+    }, [url])
+    return (data)
+}
+
+export default useRequestData;
