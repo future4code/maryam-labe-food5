9cd2f2699fcfbdd71288707c9f2533b9db43bf67
Laura Laurena <lauralaurena0204@gmail.com>
2021-11-02 20:35:51 -0300

    base dos cards 1 e 2

--- labefood/src/App.js
+++ labefood/src/App.js
@@ -7,6 +7,7 @@ import { Router } from './routers/Router';
 export default function App() {
   return (
     <div>
+
       {/* <Typography variant={'h1'}>FutureEats</Typography>
       <Button>Entrar</Button> */}
       <Router/>
--- labefood/src/pages/HomePage/HomePage.js
+++ labefood/src/pages/HomePage/HomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Card1 from '../../components/card1/Card1';
+import Card3 from '../../components/card3/Card3';
+
 
 
 export const HomePage = () => {
   return (
     <div>
-      <h1>Home</h1>
+      {/* <h1>Home</h1>
+      <Card1/>
+      <br/>
+      <Card1/>
+      <br/>
+      <Card1/>
+      <br/>
+      <Card3/> */}
+      
     </div>
   );
 }
