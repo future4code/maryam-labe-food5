8f83893d69e6b476fe5d1a5fb48fd389abfbf151
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-11-02 17:40:32 -0300

    atualizei loading da pagina inicial

--- labefood/src/pages/HomePage/HomePage.js
+++ labefood/src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { InitialPage } from '../InitialPage/InitialPage';
 
 
 export const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
+      
     </div>
   );
 }
--- labefood/src/pages/InitialPage/InitialPage.js
+++ labefood/src/pages/InitialPage/InitialPage.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { InitialPageContainer, Logo } from './styled';
 import futureEats from '../../images/futureEats.png'
-import { Button } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import { goToLoginPage } from '../../routers/Coordinator';
+
+
 
 export const InitialPage = () => {
   const history = useHistory()
+
+
+  setTimeout(function () {
+    window.location.href = '/'
+
+  }, 3000);
+
   return (
-    <><InitialPageContainer>
+    <InitialPageContainer>
       <Logo src={futureEats} />
-    </InitialPageContainer><Button onClick={()=>goToLoginPage(history)}>Entrar</Button></>
+      <CircularProgress color={'primary'}/>
+    </InitialPageContainer>
   );
 }
