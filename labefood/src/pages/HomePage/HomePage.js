f3247fa17610a770c0e84f1d4d482b011004632f
Leticia Agante de Brito <leticia.nos@gmail.com>
2021-11-03 15:32:03 -0300

    fazendo o footer

--- labefood/src/pages/HomePage/HomePage.js
+++ labefood/src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../components/footer/Footer';
 
 
 export const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Footer />
     </div>
   );
 }
